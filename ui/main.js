fb63b7870d999adf3fd7ceb90c22a231afca0700
peisheng1 <65634977+peisheng1@users.noreply.github.com>
2020-05-27 13:49:08 -0700

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,17 +1,21 @@
 var tokenizer = require('../libs/tokenizer.js');
-
+var Grid = require('../ast/Grid.js');
 function init(){
     
     // initialize tokenizer
-    var literals = ["set grid size to be","set rectangle","to","draw","from",
+    var literals = ["set grid size to be ","set rectangle","draw","from",
                     "using","with","waviness","thickness","define","as","that","place",
-                    "anywhere","times","play for","turns"];
+                    "anywhere","times","play for","turns", "(", ")", ","];
 
     // TODO: ensure input string is lowercase
-    var testInput = "make creature called perry";
+    var testInput = "set grid size to be (4, 3)";
 
     // tokenize
-    tokenizer.tokenize(literals, testInput);
+    let t = new tokenizer(literals, testInput);
+    let grid = new Grid(t);
+    grid.parse();
+    console.log(grid.width);
+    console.log(grid.height);
 }
 
 init();
