e150f261c7259bde5ebf35eb11a5ce0894b13326
peisheng1 <65634977+peisheng1@users.noreply.github.com>
2020-05-27 20:22:09 -0700

    Add files via upload

--- ui/main.js
+++ ui/main.js
@@ -1,21 +1,27 @@
 var tokenizer = require('../libs/tokenizer.js');
-var Grid = require('../ast/Grid.js');
+var GRID = require('../ast/GRID.js');
+var TERRAIN = require('../ast/TERRAIN.js');
 function init(){
     
     // initialize tokenizer
-    var literals = ["set grid size to be ","set rectangle","draw","from",
+    var literals = ["set grid size to be","set rectangle","draw","from",
                     "using","with","waviness","thickness","define","as","that","place",
                     "anywhere","times","play for","turns", "(", ")", ","];
 
     // TODO: ensure input string is lowercase
-    var testInput = "set grid size to be (4, 3)";
-
+    var testInput = "set grid size to be (40, 30) set rectangle (20, 25) (30, 35) to rock";
+    testInput = testInput.replace("(", "_(_");
+    console.log(testInput);
     // tokenize
-    let t = new tokenizer(literals, testInput);
-    let grid = new Grid(t);
+    tokenizer.makeTokenizer(literals, testInput);
+    let t = tokenizer.getTokenizer();
+    let grid = new GRID(t);
     grid.parse();
+    let terrain = new TERRAIN(t);
+    terrain.parse();
     console.log(grid.width);
     console.log(grid.height);
+    console.log(texture.topleftx);
 }
 
 init();
